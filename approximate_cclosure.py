3bf8c13d3d9cdc5d2b68fde03a4f390d159231ac
Christina Gilbert <christina.gilbert.325@gmail.com>
2016-12-21 16:07:30 -0600

    added documentation for approximate_cclosure.py

--- approximate_cclosure.py
+++ approximate_cclosure.py
@@ -1,55 +1,97 @@
+#!/usr/bin/env python
+
+__author__ = "Christina Gilbert"
+__email__ = "christyg@stanford.edu"
+__status__ = "Development"
+
 import numpy as np
 from collections import defaultdict
 from itertools import combinations
 
+MATRIX_DATA_TYPE = np.uint16
+NULL_SENTINEL = np.iinfo(np.uint16).max
 DATA_FILES = ['test.txt']
+
 
 def create_graph(filename):
     """
-    Initializes graph from data file.
+    Initializes graph from data file which maps each node to its set of neighbors.
+    We consider all graphs as undericted, all relationships are symmetrical. If
+    there is an edge between nodes 1 and 2, then 2 will be in 1's neighbor set,
+    and 1 will be in 2's neighbors set.
 
     @param string filename     name of file to create graph from
-    @return dict(int, set)     dict mapping nodes to set of nodes its connected to
+    @return dict(int, set)     dict mapping nodes to set of neighbors
     """
 
-    #print(filename + ": initializing ...")
+    print(filename + ": initializing ...")
+    graph = defaultdict(set)
+
     with open(filename, 'r') as f:
 
         tuples = [(int(line.split()[0]), int(line.split()[1]))
                   for line in f if line[0] != '#']
 
-        graph = defaultdict(set)
         for tup in tuples:
             graph[tup[0]].add(tup[1])
             graph[tup[1]].add(tup[0])
 
-        #print(filename + ": initialization complete")
-        return graph
+        f.close()
+
+    print(filename + ": initialization complete")
+    return graph
+
 
 def symmetrize_matrix(matrix, matrix_len):
+    """
+    Copies values from the top half of a triangluar matrix into the lower half
+
+    @param np.matrix matrix             matrix mapping number of common neighbors
+                                        shared by nodes
+    @param matrix_len int               number of rows, cols in square matrix
+    """
     for i in range(matrix_len):
         for j in range(i):
             matrix[i][j] = matrix[j][i]
 
-    return matrix
 
-
-#TODO: look into null types for numpy matrix in python? Cant do it right
-#now becuase no internet
 def fill_connected_nodes_in_matrix(graph, matrix, matrix_len):
+    """
+    Constructs a (symmetric) numpy matrix where matrix[i][j] is the
+    number of common neighbors shared by i and j. Connected nodes
+    and self-loops (matrix diagonal) are marked with NULL_SENTINEL
+
+    @param dict(int, set) graph         dict mapping nodes to set of neighbors
+    @paran n_nodes int                  number of nodes in graph
+
+    @return np.matrix matrix            matrix mapping number of common neighbors
+                                        shared by nodes
+    """
     for i in range(matrix_len):
-        matrix[i][i] = -1
+        matrix[i][i] = NULL_SENTINEL
 
     for node in graph.keys():
         for connected_node in graph[node]:
-            matrix[node][connected_node] = -1
+            matrix[node][connected_node] = NULL_SENTINEL
+
 
 def create_matrix(graph, n_nodes):
+    """
+    Constructs a (symmetric) numpy matrix where matrix[i][j] is the
+    number of common neighbors shared by i and j. Connected nodes
+    and self-loops are marked with NULL_SENTINEL
 
-    #the snap datasets are 1-indexed, so the matrix is 1-indexed as well
+    @param dict(int, set) graph         dict mapping nodes to set of neighbors
+    @paran n_nodes int                  number of nodes in graph
+
+    @return np.matrix matrix            matrix mapping number of common neighbors
+                                        shared by nodes
+    """
+    # the snap datasets are 1-indexed, so the matrix is 1-indexed as well
     matrix_shape = (n_nodes + 1, n_nodes + 1)
-    matrix = np.zeros(matrix_shape, dtype=np.int)
-    #remember to handle the diagonal
+    # WARNING uint16 stores values from 0 to 65535, on very large graphs this
+    # may be insufficient
+    matrix = np.zeros(matrix_shape, dtype=MATRIX_DATA_TYPE)
 
     neighbors_sets = graph.values()
 
@@ -57,14 +99,14 @@ def create_matrix(graph, n_nodes):
         neighbor_pairs = combinations(neighbor_set, 2)
 
         for pair in neighbor_pairs:
-            ordered_pair = (pair[0], pair[1]) if pair[0] < pair[1] else (pair[1], pair[0])
+            ordered_pair = (
+                pair[0], pair[1]) if pair[0] < pair[1] else (
+                pair[1], pair[0])
             matrix[ordered_pair[0], ordered_pair[1]] += 1
 
     fill_connected_nodes_in_matrix(graph, matrix, n_nodes + 1)
-    matrix = symmetrize_matrix(matrix, n_nodes + 1)
+    symmetrize_matrix(matrix, n_nodes + 1)
     return matrix
-
-
 
 if __name__ == '__main__':
     """
@@ -75,8 +117,7 @@ if __name__ == '__main__':
     for filename in DATA_FILES:
         graph = create_graph('data/' + filename)
         matrix = create_matrix(graph, len(graph.keys()))
-        print matrix 
-    #build graph
-    #build matrix
-    #remove rows in matrix until c is found
-
+        print(matrix)
+    # build graph
+    # build matrix
+    # remove rows in matrix until c is found
